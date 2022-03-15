4f4da17db9dcbc45617376e3660c38da5f3f014e
GGE2 <jihoon_story@naver.com>
2022-03-15 14:52:22 +0900

    구글 로그인 작업 완료

--- app/src/main/java/com/example/ggestagram/LoginActivity.kt
+++ app/src/main/java/com/example/ggestagram/LoginActivity.kt
@@ -11,13 +11,11 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInApi
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import java.lang.Exception
@@ -26,38 +24,76 @@ class LoginActivity : AppCompatActivity() {
 
     var auth : FirebaseAuth? = null
     var googleSignInClient : GoogleSignInClient? = null
-    private val GOOGLE_LOGIN_CODE = 9001
+    private val GOOGLE_LOGIN_CODE = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
-        login_btn1.setOnClickListener {
-            signinAndSignup()
-        }
 
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("317986852594-fjheqr2faod4ppvdlcuis7kamu0gsqqp.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this,gso)
-    }
-    fun googleLogin(){
-
-        val loginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if(it.resultCode == Activity.RESULT_OK){
-                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
-                if(task.isSuccessful){
-
-                }
-
-            }
+        //이메일 로그인
+        login_btn1.setOnClickListener {
+            signinAndSignup()
+        }
+        //구글 로그인
+        login_btn3.setOnClickListener {
+           googleLogin()
         }
 
+    }
+    fun googleLogin(){
+        var signinIntent = googleSignInClient!!.signInIntent
+        loginLauncher.launch(signinIntent)
+    }
+    private val loginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        Log.e("REQSULT CODE Start= ",it.resultCode.toString())
+        if(it.resultCode == Activity.RESULT_OK){
+            Log.e("REQSULT CODE IN= ",it.resultCode.toString())
+            Log.e("REQUEST CODE In = ",it.resultCode.toString())
+            val task = Auth.GoogleSignInApi.getSignInResultFromIntent(it.data!!)
+            if(task!!.isSuccess){
+                var account = task.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+            /*var task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try{
+                var account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+                Log.d("GoogleLogin","Firebase Auth "+account.id)
 
+            }catch(e:ApiException){
+                Log.d("GoogleLogin", "Google Login Failed")
+            }
+*/
+        }
     }
 
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)?.
+        addOnCompleteListener {
+                it->
+            if(it.isSuccessful){
+                //Login Success
+                Log.e(TAG,"signinEmail ")
+                moveMainPage(it.result.user)
+            }
+            else {
+                //Show the error message
+                Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+    }
 
 
     fun signinAndSignup(){
@@ -82,8 +118,10 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
     fun signinEmail(){
-        auth?.createUserWithEmailAndPassword(login_email_edittext.text.toString(),login_pw_edittext.text.toString())?.addOnCompleteListener {
+        auth?.createUserWithEmailAndPassword(login_email_edittext.text.toString(),login_pw_edittext.text.toString())?.
+        addOnCompleteListener {
                 it->
             if(it.isSuccessful){
                 //Login Success
