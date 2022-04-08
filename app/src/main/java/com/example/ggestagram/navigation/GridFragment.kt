337f54dd68c557676e9389a4ce7b6a3dc72e7df8
GGE2 <jihoon_story@naver.com>
2022-04-08 14:29:00 +0900

    GridFragment Recylerview 작업 완료

--- app/src/main/java/com/example/ggestagram/navigation/GridFragment.kt
+++ app/src/main/java/com/example/ggestagram/navigation/GridFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.ggestagram.R
+import com.example.ggestagram.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +27,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GridFragment : Fragment() {
+    var firestore : FirebaseFirestore? = null
+    var fragmentView : View? = null
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,10 +46,66 @@ class GridFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        firestore = FirebaseFirestore.getInstance()
+        fragmentView?.gridfragment_recyclerview?.adapter = UserFragmentRecylerView()
+        fragmentView?.gridfragment_recyclerview?.layoutManager = GridLayoutManager(activity,3)
+
+        return fragmentView
+    }
+    inner class UserFragmentRecylerView : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+
+        init{
+            firestore?.collection("images")?.addSnapshotListener { value, error ->
+
+                if(value == null) {
+                    return@addSnapshotListener
+                }
+                else{
+                    for(snapshot in value.documents){
+                        var data = snapshot.toObject(ContentDTO::class.java)
+                        contentDTOs.add(data!!)
+                    }
+
+                    notifyDataSetChanged()
+                }
+
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+            //화면 폭의 1/3
+            var width = resources.displayMetrics.widthPixels/3
+            var imageView = ImageView(parent.context)
+
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+
+            return CustomViewHolder(imageView)
+
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) : RecyclerView.ViewHolder(imageView) {
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageView = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(
+                RequestOptions().centerCrop()).into(imageView)
+
+        }
+
+        override fun getItemCount(): Int {
+
+            return contentDTOs.size
+
+        }
 
 
-        return view
     }
 
     companion object {
